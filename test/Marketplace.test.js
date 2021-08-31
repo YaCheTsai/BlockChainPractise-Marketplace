7e0db05465c523cee6b1ffbdf6df600b2c65f01e
YaCheTsai <32267451+YaCheTsai@users.noreply.github.com>
2021-08-31 13:20:27 +0800

    Add files via upload

---
+++ test/Marketplace.test.js
@@ -0,0 +1,101 @@
+const Marketplace = artifacts.require('./Marketplace.sol');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+
+contract('Marketplace', ([deployer, seller, buyer]) =>{
+    let marketplace
+
+    before(async () => {
+        marketplace = await Marketplace.deployed()
+    })
+
+    describe('deployment', async () =>{
+        it('deploys successfully', async () =>{
+            const address = await marketplace.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+
+        })
+
+        it('has a name', async () => {
+            const name = await marketplace.name()
+            assert.equal(name, 'Hello word!!')
+        })
+    })
+
+
+    describe('products', async () =>{
+        let result, productCount
+
+        before(async () => {
+            result = await marketplace.createProduct('item1', web3.utils.toWei('1', 'Ether'), { from: seller })
+            productCount = await marketplace.productCount()
+        })
+    
+
+        it('Creats prosucts', async () => {
+            
+            assert.equal(productCount, 1)
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'item1', 'name is correct')
+            assert.equal(event.price, '1000000000000000000', 'price is correct')
+            assert.equal(event.owner, seller, 'owner is correct')
+            assert.equal(event.purchased, false, 'purchased is correct')
+
+            await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
+            
+            await marketplace.createProduct('item1', 0, { from: seller }).should.be.rejected;
+        })
+
+        it('list products', async () => {
+            
+           const product = await marketplace.products(productCount)
+
+           assert.equal(product.id.toNumber(), productCount.toNumber(), 'id is correct')
+           assert.equal(product.name, 'item1', 'name is correct')
+           assert.equal(product.price, '1000000000000000000', 'price is correct')
+           assert.equal(product.owner, seller, 'owner is correct')
+           assert.equal(product.purchased, false, 'purchased is correct')
+        })
+
+        it('sell products', async () => {
+           let oldSellerBalance
+           oldSellerBalance = await web3.eth.getBalance(seller)
+           oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+
+           result = await marketplace.purchaseProduct(productCount, {from: buyer, value: web3.utils.toWei('1', 'Ether')})
+
+           const event = result.logs[0].args
+           assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+           assert.equal(event.name, 'item1', 'name is correct')
+           assert.equal(event.price, '1000000000000000000', 'price is correct')
+           assert.equal(event.owner, buyer, 'owner is correct')
+           assert.equal(event.purchased, true, 'purchased is correct')
+
+           let newSellerBalance 
+           newSellerBalance = await web3.eth.getBalance(seller)
+           newSellerBalance = new web3.utils.BN(newSellerBalance)
+
+           let price = web3.utils.toWei('1', 'Ether')
+           price = new web3.utils.BN(price)
+
+           const expectedBalance = oldSellerBalance.add(price)
+           assert.equal(newSellerBalance.toString(), expectedBalance.toString(), 'balance is correct')
+            
+           await marketplace.purchaseProduct(productCount+1).should.be.rejected;
+            
+           await marketplace.purchaseProduct(productCount, {from: buyer, value: 10}).should.be.rejected;
+           await marketplace.purchaseProduct(productCount, {from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+           await marketplace.purchaseProduct(productCount, {from: deployer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+           await marketplace.purchaseProduct(productCount, {from: seller, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+
+         })
+    })
+
+})
